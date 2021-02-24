0cddd18a76911d7be26facf9cf3ae0fba03a61f7
ligaya <ligaya96@gmail.com>
2021-02-24 00:19:35 -0800

    added some JS

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>
+inquirer.prompt([
+
+// questions: "What is your GitHub username?"
+// questions: "What is your email address?"
+// questions: "What is your project's name?"
+// questions: "Please write a short description of your project"
+// questions: "What kind of license should your project have?",
+// choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+//   },
+// questions: "What command should be run to install dependencies?",
+// default: "npm i"
+// questions: "What command should be run to run tests?",
+// default: "npm test"
+// questions: "What does the user need to know about using the repo?",
+// message: "What does the user need to know about contributing to the repo?"
+    
+    
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
