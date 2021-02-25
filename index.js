c5967e85cc10db954584da766956b3fe2040b048
ligaya <ligaya96@gmail.com>
2021-02-24 19:32:12 -0800

    added HTML file

--- index.js
+++ index.js
@@ -5,23 +5,59 @@ const fs = require("fs")
 // TODO: Create an array of questions for user input
 const questions = () =>
 inquirer.prompt([
-
-// questions: "What is your GitHub username?"
-// questions: "What is your email address?"
-// questions: "What is your project's name?"
-// questions: "Please write a short description of your project"
-// questions: "What kind of license should your project have?",
-// choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-//   },
-// questions: "What command should be run to install dependencies?",
-// default: "npm i"
-// questions: "What command should be run to run tests?",
-// default: "npm test"
-// questions: "What does the user need to know about using the repo?",
-// message: "What does the user need to know about contributing to the repo?"
-    
-    
-])
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write quick description of your project",
+    },
+    // {
+    //     type: "options",
+    //     name: "license",
+    //     message: "What type of license is included in your project?",
+    // },
+]);
+const generateHTML = (questions) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>ReadME Generator</title>
+</head>
+<body>
+    <div class="jumbotron">
+        <h1> Professional ReadME Generator!</h1>
+        <div class = "content">
+            <ul>
+                <li>
+                     <p> Github Username: ${questions.username}</p>
+                     <p> Email address: ${questions.email}</p>
+                     <p> ${questions.title}</p>
+                     <p> ${questions.description}</p>
+                    //  <p>What kind of license should your project have? ${questions.license}</p>
+                    // Badges ${questions.badges}
+                </li>
+            </ul>
+         </div>
+</body>
+</html>`;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
